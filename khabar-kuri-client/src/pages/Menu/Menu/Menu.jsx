58b1a7405d474e22b47d7ab2bc362efe24ecaa6f
najmul <mdnajmulislam10992@gmail.com>
2025-05-03 22:13:10 +0600

    implement cover with blur paralax using react paralax

--- khabar-kuri-client/src/pages/Menu/Menu/Menu.jsx
+++ khabar-kuri-client/src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/menu-bg.png'
+import PopularMenue from "../../Home/PopularMenue/PopularMenue";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>KhabarKuri | Menu</title>
       </Helmet>
+      
+      <Cover
+      img={menuImg}
+      title="Our Menu"
+      ></Cover>
+      <PopularMenue></PopularMenue>
+      <Cover
+      img={menuImg}
+      title="Our Menu"
+      ></Cover>
+      <PopularMenue></PopularMenue>
+      <Cover
+      img={menuImg}
+      title="Our Menu"
+      ></Cover>
+      <PopularMenue></PopularMenue>
     </div>
   );
 };
