4e563e37031a0b4c49a580731b81a2d5afc5df64
najmul <mdnajmulislam10992@gmail.com>
2025-05-05 17:30:39 +0600

    delte cart item and async data using refetch

--- khabar-kuri-client/src/Layout/DashBoard.jsx
+++ khabar-kuri-client/src/Layout/DashBoard.jsx
@@ -1,57 +1,72 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
+import {
+  FaAd,
+  FaCalendar,
+  FaHome,
+  FaList,
+  FaSearch,
+  FaShoppingCart,
+} from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
-
+import useCart from "../Hooks/useCart";
 
 const DashBoard = () => {
-    return (
-        <div className="flex">
-            {/* dahsboard sidebar */}
-            <div className="w-64 min-h-screen bg-orange-400">
-                <ul className="menu">
-                    <li>
-                        <NavLink to='/dashboard/userHome'>
-                        <FaHome className=""></FaHome>
-                        User Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/reservation'>
-                        <FaCalendar className=""></FaCalendar>
-                        Reservation</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/cart'>
-                        <FaShoppingCart className=""></FaShoppingCart>
-                        My Cart</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/review'>
-                        <FaAd className=""></FaAd>
-                        Review</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/dashboard/bookings'>
-                        <FaList className=""></FaList>
-                        My Bookings</NavLink>
-                    </li>
-                    <div className="divider"></div>
-                    <li>
-                        <NavLink to='/'>
-                        <FaHome className=""></FaHome>
-                        Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/order/salad'>
-                        <FaSearch className=""></FaSearch>
-                        Menu</NavLink>
-                    </li>
-                </ul>
-            </div>
-            {/* dahsboard content */}
-            <div className="flex-1 p-8">
-                <Outlet></Outlet>
-            </div>
-        </div>
-    );
+  const [cart] = useCart();
+  return (
+    <div className="flex">
+      {/* dahsboard sidebar */}
+      <div className="w-56 min-h-screen bg-orange-400">
+        <ul className="menu">
+          <li>
+            <NavLink to="/dashboard/userHome">
+              <FaHome className=""></FaHome>
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              <FaCalendar className=""></FaCalendar>
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              <FaShoppingCart className=""></FaShoppingCart>
+              My Cart: ({cart.length})
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+              <FaAd className=""></FaAd>
+              Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+              <FaList className=""></FaList>
+              My Bookings
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome className=""></FaHome>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <FaSearch className=""></FaSearch>
+              Menu
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      {/* dahsboard content */}
+      <div className="flex-1 p-8 w-full">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
--- khabar-kuri-client/src/pages/Dashboard/Cart/Cart.jsx
+++ khabar-kuri-client/src/pages/Dashboard/Cart/Cart.jsx
@@ -1,26 +1,55 @@
+import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../../Hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart] = useCart();
+  const [cart, refecth] = useCart();
   const totalPrice = cart.reduce(
     (sum, currentItem) => sum + currentItem.price,
     0
   );
+  const axiosSecure = useAxiosSecure();
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount) {
+            refecth();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
-      <div className="flex justify-evenly  ">
+      <div className="flex justify-evenly  mb-8 ">
         <h2 className="text-4xl">Items: {cart.length}</h2>
         <h2 className="text-4xl">Total Price: {totalPrice}</h2>
         <button className="btn btn-primary ">Pay: {totalPrice}</button>
       </div>
-      <div className="overflow-x-auto">
-        <table className="table">
+      <div className="overflow-x-auto ">
+        <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
-              <th>
-               #
-              </th>
+              <th>#</th>
               <th>Image</th>
               <th>Name</th>
               <th>Price</th>
@@ -28,13 +57,9 @@ const Cart = () => {
             </tr>
           </thead>
           <tbody>
-            {cart.map((item) => (
+            {cart.map((item, indx) => (
               <tr key={item._id}>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
+                <th>{indx + 1}</th>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -45,26 +70,20 @@ const Cart = () => {
                         />
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
                   </div>
                 </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
+                <td>{item.name}</td>
+                <td>${item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-rose-700"></FaTrashAlt>
+                  </button>
                 </th>
               </tr>
             ))}
-           
           </tbody>
         </table>
       </div>
