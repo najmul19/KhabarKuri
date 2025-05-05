a36c9f0f0108e4f795e453d39baf3ce6e0db51c4
najmul <mdnajmulislam10992@gmail.com>
2025-05-05 16:46:02 +0600

    create dashboard route and dashboard navigation

---
+++ khabar-kuri-client/src/Layout/DashBoard.jsx
@@ -0,0 +1,46 @@
+import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { NavLink, Outlet } from "react-router-dom";
+
+
+const DashBoard = () => {
+    return (
+        <div className="flex">
+            {/* dahsboard sidebar */}
+            <div className="w-64 min-h-screen bg-orange-400">
+                <ul className="menu">
+                    <li>
+                        <NavLink to='/dashboard/userHome'>
+                        <FaHome className=""></FaHome>
+                        User Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/reservation'>
+                        <FaCalendar className=""></FaCalendar>
+                        Reservation</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/cart'>
+                        <FaShoppingCart className=""></FaShoppingCart>
+                        My Cart</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/review'>
+                        <FaAd className=""></FaAd>
+                        Review</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/bookings'>
+                        <FaList className=""></FaList>
+                        My Bookings</NavLink>
+                    </li>
+                </ul>
+            </div>
+            {/* dahsboard content */}
+            <div className="flex-1">
+                <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default DashBoard;
--- khabar-kuri-client/src/Routes/Routes.jsx
+++ khabar-kuri-client/src/Routes/Routes.jsx
@@ -7,6 +7,8 @@ import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import Secret from "../pages/Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
+import DashBoard from "../Layout/DashBoard";
+import Cart from "../pages/Dashboard/Cart/Cart";
 
 export const router = createBrowserRouter([
   {
@@ -39,4 +41,14 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: '/dashboard',
+    element: <DashBoard></DashBoard>,
+    children: [
+      {
+        path: 'cart',
+        element: <Cart></Cart>
+      }
+    ]
+  }
 ]);
