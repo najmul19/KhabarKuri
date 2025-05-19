322163d54e87ac68915b936c22b3e87ce6c89a20
najmul <mdnajmulislam10992@gmail.com>
2025-05-20 01:23:00 +0600

    delete menu item with verify admin midlware

--- khabar-kuri-client/src/Hooks/useMenu.jsx
+++ khabar-kuri-client/src/Hooks/useMenu.jsx
@@ -1,18 +1,35 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-  const [menu, setMenue] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetch("http://localhost:5000/menu")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenue(data);
-        setLoading(false);
-      });
-  }, []);
+  // const [menu, setMenue] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenue(data);
+  //       setLoading(false);
+  //     });
+  // }, []);
 
-  return [menu,loading];
+  // 5/20/2025
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: menu = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/menu");
+      return res.data;
+    },
+  });
+
+  return [menu, loading, refetch];
 };
 
 export default useMenu;
--- khabar-kuri-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ khabar-kuri-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,12 +1,40 @@
 import { FaEdit, FaTrashAlt, FaUsers } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../Hooks/useMenu";
-
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+// 5/20/2025
 const ManageItems = () => {
-  const [menu] = useMenu();
-  const handleDeleteItem=(item) =>{
+  const [menu, , refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
-  }
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        // console.log(res.data);
+        if (res.data.deletedCount) {
+          // refetch to update the ui
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${item.name} hase been deleted`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }
+    });
+  };
   return (
     <div>
       <SectionTitle
@@ -28,12 +56,10 @@ const ManageItems = () => {
               </tr>
             </thead>
             <tbody>
-              {menu.map((item,indx) => (
+              {menu.map((item, indx) => (
                 <tr key={item._id}>
                   <th>
-                    <label>
-                      {indx+1}
-                    </label>
+                    <label>{indx + 1}</label>
                   </th>
                   <td>
                     <div className="flex items-center gap-3">
@@ -51,19 +77,19 @@ const ManageItems = () => {
                   <td className="text-right">${item.price}</td>
                   <td>
                     <button
-                          // onClick={() => handleMakeAdmin(user)}
-                          className="btn btn-ghost bg-orange-500 btn-sm"
-                        >
-                          <FaEdit className="text-white  "></FaEdit>
-                        </button>
+                      // onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost bg-orange-500 btn-sm"
+                    >
+                      <FaEdit className="text-white  "></FaEdit>
+                    </button>
                   </td>
                   <td>
                     <button
-                        onClick={() => handleDeleteItem(item)}
-                        className="btn btn-ghost btn-lg"
-                      >
-                        <FaTrashAlt className="text-rose-700"></FaTrashAlt>
-                      </button>
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-lg"
+                    >
+                      <FaTrashAlt className="text-rose-700"></FaTrashAlt>
+                    </button>
                   </td>
                 </tr>
               ))}
