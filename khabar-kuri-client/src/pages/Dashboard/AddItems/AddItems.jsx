29318a2ebcb3dc11252326c185bdb78fe9ef15d1
najmul <mdnajmulislam10992@gmail.com>
2025-05-19 23:39:42 +0600

    upload image to image hosting server imgbb and get image url

--- khabar-kuri-client/src/pages/Dashboard/AddItems/AddItems.jsx
+++ khabar-kuri-client/src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,10 +1,24 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+//5-19-2025
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const axiosPublic = useAxiosPublic();
+  const onSubmit = async (data) => {
     console.log(data);
+    // image upload to imagbb and then get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
   };
   return (
     <div>
@@ -33,10 +47,11 @@ const AddItems = () => {
                 <span className="label-text">Category*</span>
               </div>
               <select
+                defaultValue="default"
                 {...register("category", { required: true })}
                 className="select select-bordered w-full "
               >
-                <option disabled selected>
+                <option disabled value="default">
                   Select A Category
                 </option>
                 <option value="salad">Salad</option>
@@ -73,7 +88,7 @@ const AddItems = () => {
             ></textarea>
           </label>
           <input
-            {...register("iamge", { required: true })}
+            {...register("image", { required: true })}
             type="file"
             className="file-input w-full"
           />
