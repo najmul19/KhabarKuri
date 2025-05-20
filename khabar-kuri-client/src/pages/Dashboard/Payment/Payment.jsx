a987e9e26a9b6344f31405f3a6b181cc6005a7ce
najmul <mdnajmulislam10992@gmail.com>
2025-05-20 13:52:42 +0600

    install stripe for react and create card eleemnt

--- khabar-kuri-client/src/pages/Dashboard/Payment/Payment.jsx
+++ khabar-kuri-client/src/pages/Dashboard/Payment/Payment.jsx
@@ -1,5 +1,13 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-
+import {
+  PaymentElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+//5/20/2025
+// add publishable key
+const stripePromise = loadStripe('');
 const Payment = () => {
     return (
         <div>
@@ -7,6 +15,11 @@ const Payment = () => {
             heading="Payment"
             subHeading="Please pay to eat"
             ></SectionTitle>
+            <div>
+                <Elements stripe={stripePromise}>
+                    
+                </Elements>
+            </div>
         </div>
     );
 };
