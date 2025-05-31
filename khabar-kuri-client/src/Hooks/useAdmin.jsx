c99b534bfd1c33cbd6be7cfbceb4738f65dea9e0
najmul <mdnajmulislam10992@gmail.com>
2025-05-31 22:03:32 +0600

    fix login and api race condition

--- khabar-kuri-client/src/Hooks/useAdmin.jsx
+++ khabar-kuri-client/src/Hooks/useAdmin.jsx
@@ -3,10 +3,11 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 //5/15/2025
 const useAdmin = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
   const { data: isAdmin,isPending: isAdminLoading } = useQuery({
     queryKey: [user?.email, "isAdmin"],
+    enabled: !loading,
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/admin/${user.email}`);
       console.log(res.data);
